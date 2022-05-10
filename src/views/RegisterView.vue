399c3d6a2e6682a25ac3bb54745b3647b0f99c9d
Juanideni <juanidenipoti11@gmail.com>
2022-05-10 11:33:09 -0300

    first commit

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Register msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Register from '@/components/Register.vue'
+
+export default {
+  name: 'RegisterView',
+  components: {
+    Register
+  }
+}
+</script>
